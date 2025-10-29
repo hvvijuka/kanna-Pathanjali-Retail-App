40a5fa577fffab1af082d691bc744898f4f5bc46
VijayaKumari H. V <vijayahvofficial@gmail.com>
2025-10-29 09:25:49 +0530

    hash

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // ✅ Safe to uncomment
-
-const isGithubPages = window.location.pathname.startsWith("/kanna-Pathanjali-Retail-App");
-const basename = isGithubPages ? "/kanna-Pathanjali-Retail-App" : "/";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
